aff3d6f55eb0240f84856a005bf98487b4486ca5
AmandynhaM001 <101841247+AmandynhaM001@users.noreply.github.com>
2025-05-14 18:33:02 -0300

    script

--- scripts.js
+++ scripts.js
@@ -1,4 +1,45 @@
-const uploadBtn = document.getElementById("uploadBtn");
+const uploadBtn = document.getElementById("upload-btn");
 const inputUpload = document.getElementById("image-upload");
 
-uploadBtn.addEventListener("")
+uploadBtn.addEventListener("click", () => {
+    inputUpload.click();
+})
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise ((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name})
+        }
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+        leitor.readAsDataURL(arquivo)
+    })
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch (erro) {
+            console.error("Erro na leitura do arquivo")
+        }
+    }
+})
+
+const inputTags = document.getElementById("input-tags");
+
+inputTags.addEventListener("keypress",(evento) => {
+    if (evento.key === "Enter") {
+        evento.preventDefault();
+        const tagText = tagsInput.value.trim();
+        if (tagText)
+    }
+})
